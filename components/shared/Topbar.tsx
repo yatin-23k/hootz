f77e31f9cf63c5dcdd7ce30a5b7bd8828e73d94b
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-06-05 23:26:32 +0530

    implement onBoarding page

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from '@clerk/themes'
 
 function Topbar() {
     return (
@@ -15,7 +16,7 @@ function Topbar() {
                     <SignedIn>
                         <SignOutButton>
                             <div className="flex cursor-pointer">
-                                <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+                                <Image src="/assets/logout.svg" alt="logout" width={24} height={24} className="hover:scale-110" />
                             </div>
                         </SignOutButton>
                     </SignedIn>
@@ -23,6 +24,7 @@ function Topbar() {
 
                 <OrganizationSwitcher 
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger: "py-2 px-4"
                         }
