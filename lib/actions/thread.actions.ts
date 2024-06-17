00dd3a3fa1ef5c10a1c2c82c4da7a5dffcb36329
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-06-17 06:36:59 +0530

    implement thread frontend and backend

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -32,3 +32,61 @@ export async function createThread({ text, author, communityId, path }: Params) 
 
     
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] }})
+                                    .sort({ createdAt: 'desc' })
+                                    .skip(skipAmount)
+                                    .limit(pageSize)
+                                    .populate({ path: 'author', model: User })
+                                    .populate({ path: 'children', 
+                                        populate: { path: 'author', model: User, select: "_id name parentId image" }})
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] }})
+    
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string){
+    connectToDB();
+
+    try{
+        const thread = await Thread.findById(id)
+                                    .populate({
+                                        path: "author",
+                                        model: User,
+                                        select: "_id id name image"
+                                    })
+                                    .populate({
+                                        path: 'children',
+                                        populate: [
+                                            {
+                                                path: 'author',
+                                                model: User,
+                                                select: "_id id name parentId image"
+                                            },
+                                            {
+                                                path: 'children',
+                                                model: Thread,
+                                                populate: {
+                                                    path: 'author',
+                                                    model: User,
+                                                    select: "_id id name parentId image"
+                                                }
+                                            }
+                                        ]
+                                    }).exec()
+    
+        return thread;              
+    } catch(error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+    }
+}
