5aff95c24b4a036566b0d78d4c9ec30dd90c92d9
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-06-19 05:29:37 +0530

    implement search

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,11 +1,48 @@
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import UserCard from "@/components/cards/UserCard";
 
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
 
-const Page = async () => {
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25
+  })
+
   return (
     <section>
-        <h1 className="head-text mb-10">Search</h1>
+      <h1 className="head-text mb-10">Search</h1>
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+            <p className="no-result">No users</p>
+        ) : (
+            <>
+                {result.users.map((person) => (
+                    <UserCard
+                      key={person.id}
+                      id={person.id}
+                      name={person.name}
+                      username={person.username}
+                      imgUrl={person.image}
+                      personType='User'
+                    />
+                ))}
+            </>
+        )}
+      </div>
     </section>
-  )
+  );
 }
 
 export default Page;
